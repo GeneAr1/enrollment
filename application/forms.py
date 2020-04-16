1167d7e1521a86be324b05fd3f903bd2a4455d81
Gene Arsenault <geneaar@outlook.com>
2020-04-16 16:08:11 -0400

    Ver 3.1 Process forms and Data update
    1 modules ot hash and check passwords in  user model
    
    2 module to validate password in forms
    
    3 fix inport lines in model and forms
    
    4 minor changes ot register and login forms

--- application/forms.py
+++ application/forms.py
@@ -1,18 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from application import User
 
 
 class Loginform(FlaskForm):
-    email       =   StringField("Email", validators=[DataRequired()])
-    password    =   StringField("Password", validators=[DataRequired()])
+    email       =   StringField("Email", validators=[DataRequired(), Email()])
+    password    =   StringField("Password", validators=[DataRequired(), Length(min=6,max=20)])
     remember_me =   BooleanField("Remember Me")
     submit      =   SubmitField("Login")
 
+
 class RegisterForm(FlaskForm):
-    email       =   StringField("Email", validators=[DataRequired()])
-    password    =   StringField("Password", validators=[DataRequired()])
-    password_confirm    =   StringField("Password Confirm", validators=[DataRequired()])
-    first_name    =   StringField("First Name", validators=[DataRequired()])
-    last_name    =   StringField("Last Name", validators=[DataRequired()])
+    email       =   StringField("Email", validators=[DataRequired(), Email()])
+    password    =   StringField("Password", validators=[DataRequired(), Length(min=6,max=20)])
+    password_confirm    =   StringField("Password Confirm", validators=[DataRequired(), Length(min=6,max=20), EqualTo('password')])
+    first_name    =   StringField("First Name", validators=[DataRequired(), Length(min=2,max=50)])
+    last_name    =   StringField("Last Name", validators=[DataRequired(), Length(min=2, max=50)])
     submit      =   SubmitField("Register Now")
+    
+
+#email validation module
+    def validate_email(self,email):
+        user = User.objects(email=email.data).first()
+        if user:
+            raise ValidationError("Email is already in use.  Please use different email")
